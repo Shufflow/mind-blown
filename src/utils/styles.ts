66921213f3629eab912b40a3bf643e61a5630e42
Flávio Caetano <caetano@work.co>
2018-12-20 19:06:22 -0200

    feat(Style): add colors to global styles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,4 +1,16 @@
+export const Colors = {
+  black: 'black',
+  gray: '#AAA',
+  lightGray: '#D0D0D0',
+  offWhite: '#F8F8F8',
+  white: 'white',
+};
+
 export const Layouts: any = {
+  bottomBorder: (borderColor: string = Colors.offWhite) => ({
+    borderColor,
+    borderBottomWidth: 1,
+  }),
   container: {
     flex: 1,
   },
@@ -17,6 +29,7 @@ export const Layouts: any = {
 export const Spacing = {
   size_0: 0,
   size_10: 10,
+  size_15: 15,
   size_20: 20,
   size_30: 30,
   size_40: 40,
