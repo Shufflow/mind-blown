67b55964e0f5b61d014499f58e3ee41951555110
Flávio Caetano <caetano@work.co>
2019-01-08 17:51:25 -0200

    fix(Locale): use stateless component in withLocale hoc

--- src/utils/hocs/withLocale.tsx
+++ src/utils/hocs/withLocale.tsx
@@ -43,35 +43,14 @@ export const withLocaleProvider = <T extends Object>(
   return LocaleProvider;
 };
 
-/**
- * Typescript has an error where it can't handle Partial with generic types.
- * This is a workaround to make `locale` and `setLocale` optional, so Typescript doesn't complain
- * about components using components wrapped by `withLocale`
- */
-interface PrivateConsumer {
-  locale?: string;
-  setLocale?: (locale: string) => void;
-}
-
 export const withLocale = <T extends Object>(
   WrappedComponent: React.ComponentType<T>,
-): React.ComponentClass<any> => {
-  // tslint:disable-next-line:max-classes-per-file
-  class LocaleConsumer extends React.PureComponent<T & PrivateConsumer> {
-    render() {
-      return (
-        <Consumer>
-          {({ locale, setLocale }) => (
-            <WrappedComponent
-              locale={locale}
-              setLocale={setLocale}
-              {...this.props}
-            />
-          )}
-        </Consumer>
-      );
-    }
-  }
-
-  return LocaleConsumer;
-};
+): React.ComponentType<T & LocaleConsumerProps> => (
+  props: T,
+): React.ReactElement<T> => (
+  <Consumer>
+    {({ locale, setLocale }) => (
+      <WrappedComponent locale={locale} setLocale={setLocale} {...props} />
+    )}
+  </Consumer>
+);
