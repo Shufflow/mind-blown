efc6d17981955e3358a2da550f3d494b1784b156
Flávio Caetano <flavio@vieiracaetano.com>
2019-01-13 18:34:33 -0200

    fix(Fonts): removing troublesome fonts

--- src/routes/Home/components/Phrase/styles.ts
+++ src/routes/Home/components/Phrase/styles.ts
@@ -5,6 +5,7 @@ import { Spacing } from 'src/utils/styles';
 const styles = StyleSheet.create({
   text: {
     fontSize: Spacing.size_40,
+    lineHeight: Spacing.size_45,
     padding: Spacing.size_30,
     textAlign: 'center',
   },
