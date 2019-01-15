11c35d61bcee35bfd3fb64f84793b3eec166a902
Flávio Caetano <caetano@work.co>
2019-01-15 14:37:29 -0200

    fix(Android): fix crash on Android launch due to react-native-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // tslint:disable:file-name-casing
 
 import React from 'react';
+import { Platform } from 'react-native';
 import { useScreens } from 'react-native-screens';
 
 import AppNavigator from 'src/navigators/AppNavigator';
@@ -8,7 +9,7 @@ import firebaseInit from 'src/models/firebase';
 
 import { withLocaleProvider } from 'src/utils/hocs/withLocale';
 
-useScreens();
+useScreens(Platform.OS === 'ios');
 firebaseInit();
 
 const App = (): React.ReactElement<{}> => <AppNavigator />;
