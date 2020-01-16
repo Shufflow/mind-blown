e988014316f167a1ab5cd262b7aa4c10fce5d677
Flávio Caetano <flavio@vieiracaetano.com>
2020-01-16 17:07:53 -0500

    chore(Dependencies): install array shuffler

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -7,3 +7,7 @@ declare module 'base-64' {
   const decode: (input: string) => string;
   const encode: (input: string) => string;
 }
+
+declare module 'knuth-shuffle' {
+  const knuthShuffle: <T>(array: Array<T>) => Array<T>;
+}
